491593c62e20134eae5fcc03ee073ce097ce1be0
Yu Cao <ycao@us.ibm.com>
2018-04-23 21:28:14 -0400

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # hcm-ui-api
+
+## Running
+1. The folloing environment variables need to be set.
+```
+hcmUrl
+```
+2. Start the server
+```
+npm start[:production]
+```
--- src/server.js
+++ src/server.js
@@ -2,8 +2,8 @@ import express from 'express';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import schema from './src/schema/index';
-import config from './config';
+import schema from './schema/index';
+import config from '../config';
 
 const GRAPHQL_PORT = process.env.PORT || config.get('httpPort') || 4000;
 
@@ -15,6 +15,6 @@ graphQLServer.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
 graphQLServer.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 
 graphQLServer.listen(GRAPHQL_PORT, () => {
-  console.log(`HCM UI API is now running on http://localhost:${GRAPHQL_PORT}/graphql`)
-  console.log(`GraphiQL is now running on http://localhost:${GRAPHQL_PORT}/graphiql`)
+  console.log(`HCM UI API is now running on http://localhost:${GRAPHQL_PORT}/graphql`);
+  console.log(`GraphiQL is now running on http://localhost:${GRAPHQL_PORT}/graphiql`);
 });
