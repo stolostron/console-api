98697b9e3089505c69ff94bb04c342764e1e6b59
Kevin Cormier <kcormier@redhat.com>
2020-04-17 15:19:46 -0400

    Disable findings resolver (currently unused)

--- src/v2/models/findings.js
+++ src/v2/models/findings.js
@@ -9,85 +9,84 @@
  ****************************************************************************** */
 
 // import _ from 'lodash';
-import KubeModel from './kube';
 // import config from '../../../config';
+import KubeModel from './kube';
 
 export default class SFModel extends KubeModel {
-  static getOccurrences(/* args */) {
-    // const url = config.get('NODE_ENV') === 'test' ? 'http://0.0.0.0' : config.get('cfcRouterUrl');
-    // const iamToken = _.get(args.req, "cookies['acm-access-token-cookie']") ||
-    //   config.get('acm-access-token-cookie');
-    // const opts = {
-    //   url: `${url}/findings/v1/id-mycluster-account/graph`,
-    //   headers: {
-    //     AccessToken: iamToken,
-    //   },
-    // };
-    // const body = {
-    //   query: `
-    //   {
-    //     occurrences(kind: "FINDING") {
-    //       name
-    //       noteName
-    //       updateTime
-    //       createTime
-    //       shortDescription
-    //       context {
-    //         accountId
-    //         region
-    //         resourceType
-    //         resourceName
-    //         resourceId
-    //         resourceCrn
-    //         serviceName
-    //         serviceCrn
-    //         clusterName
-    //         namespaceName
-    //       }
-    //       reportedBy {
-    //         id
-    //         title
-    //         url
-    //       }
-    //       finding {
-    //         severity
-    //         certainty
-    //         networkConnection {
-    //           client {
-    //             address
-    //             port
-    //           }
-    //           server {
-    //             address
-    //             port
-    //           }
-    //           direction
-    //           protocol
-    //         }
-    //         nextSteps {
-    //           title
-    //           url
-    //         }
-    //         dataTransferred {
-    //           clientBytes
-    //           clientPackets
-    //           serverBytes
-    //           serverPackets
-    //         }
-    //       }
-    //       securityClassification {
-    //         securityStandards
-    //         securityCategories
-    //         securityControl
-    //       }
-    //     }
-    //   }`,
-    // };
-    // const response = await this.kubeConnector.post('', body, opts);
-    // if (response.errors) {
-    //   throw new Error(`OCM ERROR ${JSON.stringify(response.errors)}`);
-    // }
-    // return response.data.occurrences || [];
-    return [];
-  }
+  // async getOccurrences(args) {
+  //   const url = config.get('NODE_ENV') === 'test' ? 'http://0.0.0.0' : config.get('cfcRouterUrl');
+  //   const iamToken = _.get(args.req, "cookies['acm-access-token-cookie']") ||
+  //     config.get('acm-access-token-cookie');
+  //   const opts = {
+  //     url: `${url}/findings/v1/id-mycluster-account/graph`,
+  //     headers: {
+  //       AccessToken: iamToken,
+  //     },
+  //   };
+  //   const body = {
+  //     query: `
+  //     {
+  //       occurrences(kind: "FINDING") {
+  //         name
+  //         noteName
+  //         updateTime
+  //         createTime
+  //         shortDescription
+  //         context {
+  //           accountId
+  //           region
+  //           resourceType
+  //           resourceName
+  //           resourceId
+  //           resourceCrn
+  //           serviceName
+  //           serviceCrn
+  //           clusterName
+  //           namespaceName
+  //         }
+  //         reportedBy {
+  //           id
+  //           title
+  //           url
+  //         }
+  //         finding {
+  //           severity
+  //           certainty
+  //           networkConnection {
+  //             client {
+  //               address
+  //               port
+  //             }
+  //             server {
+  //               address
+  //               port
+  //             }
+  //             direction
+  //             protocol
+  //           }
+  //           nextSteps {
+  //             title
+  //             url
+  //           }
+  //           dataTransferred {
+  //             clientBytes
+  //             clientPackets
+  //             serverBytes
+  //             serverPackets
+  //           }
+  //         }
+  //         securityClassification {
+  //           securityStandards
+  //           securityCategories
+  //           securityControl
+  //         }
+  //       }
+  //     }`,
+  //   };
+  //   const response = await this.kubeConnector.post('', body, opts);
+  //   if (response.errors) {
+  //     throw new Error(`OCM ERROR ${JSON.stringify(response.errors)}`);
+  //   }
+  //   return response.data.occurrences || [];
+  // }
 }
