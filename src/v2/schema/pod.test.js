096f9f2e75b5d59914e66aba23b491b47363d3cb
Zack Layne <zlayne@redhat.com>
2020-05-29 16:02:18 -0400

    Add new pod logs call for search details page

--- src/v2/schema/pod.test.js
+++ src/v2/schema/pod.test.js
@@ -52,7 +52,7 @@ describe('Pods Resolver', () => {
       .send({
         query: `
         {
-          logs(containerName: "hcm-ui", podName:"mcm-ibm-mcm-prod-mcmui-67f7f87d9d-5zr2b", podNamespace:"kube-system", clusterName:"cluster1")
+          logs(containerName: "search-api", podName:"search-prod-28a0e-search-api-66cf776db5-7bzfh", podNamespace:"open-cluster-management", clusterName:"cluster1")
         }
       `,
       })
