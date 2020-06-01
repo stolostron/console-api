d75248ddb9726e7ba2c920bb75c76b28175daa8d
jswanke <jswanke@JSWANKE-W10E.win.redhat.com>
2020-06-01 16:30:42 -0400

    fix lint

--- src/v2/models/clusterImport.js
+++ src/v2/models/clusterImport.js
@@ -10,7 +10,6 @@
  * Copyright (c) 2020 Red Hat, Inc.
  ****************************************************************************** */
 
-import _ from 'lodash';
 import logger from '../lib/logger';
 
 export default class ClusterImportModel {
