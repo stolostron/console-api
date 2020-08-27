fda3122f11b76dc5d6a3b53615d0506b0cb7e775
Feng Xiang <fxiang@redhat.com>
2020-08-27 15:05:31 -0400

    Add unit test

---
+++ src/v2/schema/user-access-all-namespaces.test.js
@@ -0,0 +1,24 @@
+/** *****************************************************************************
+ * Copyright (c) 2020 Red Hat, Inc.
+ ****************************************************************************** */
+
+import supertest from 'supertest';
+import server, { GRAPHQL_PATH } from '../index';
+
+describe('User Access All Namespaces Resolver', () => {
+  test('Correctly Resolves User Access All Namespaces Query', () => new Promise((done) => {
+    supertest(server)
+      .post(GRAPHQL_PATH)
+      .send({
+        query: `
+          {
+            userAccessAllNamespaces(resource:"pods", action:"delete", apiGroup:"")
+          }
+      `,
+      })
+      .end((err, res) => {
+        expect(JSON.parse(res.text)).toMatchSnapshot();
+        done();
+      });
+  }));
+});
