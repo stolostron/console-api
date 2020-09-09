42c0645639b12bf68812e352e1aa9bdf2a1348c8
Sho Weimer <sweimer@redhat.com>
2020-09-09 15:24:52 -0400

    update tests for back-end syncing

--- src/v2/mocks/ClusterManagementAddons.js
+++ src/v2/mocks/ClusterManagementAddons.js
@@ -9,53 +9,95 @@ const mockResponse = {
     apiVersion: 'addon.open-cluster-management.io/v1alpha1',
     kind: 'ClusterManagementAddOn',
     metadata: {
-      creationTimestamp: '2020-08-31T23:54:01Z',
+      creationTimestamp: '2020-09-09T17:48:35Z',
       generation: 1,
       managedFields: [{
-        apiVersion: 'addon.open-cluster-management.io/v1alpha1',
-        fieldsType: 'FieldsV1',
-        fieldsV1: {
-          'f:spec': {
-            '.': {}, 'f:addOnConfigCRD': {}, 'f:description': {}, 'f:displayName': {},
-          },
-        },
-        manager: 'Mozilla',
-        operation: 'Update',
-        time: '2020-08-31T23:54:01Z',
+        apiVersion: 'addon.open-cluster-management.io/v1alpha1', fieldsType: 'FieldsV1', fieldsV1: { 'f:spec': { '.': {}, 'f:addOnConfiguration': { '.': {}, 'f:crName': {}, 'f:crdName': {} }, 'f:addOnMeta': { '.': {}, 'f:description': {}, 'f:displayName': {} } }, 'f:status': {} }, manager: 'endpoint-operator', operation: 'Update', time: '2020-09-09T17:48:35Z',
       }],
-      name: 'observability-addon',
-      resourceVersion: '5885884',
-      selfLink: '/apis/addon.open-cluster-management.io/v1alpha1/clustermanagementaddons/observability-addon',
-      uid: '196ec5b4-5e06-4ad0-9440-31f08ee7c252',
+      name: 'application-manager',
+      resourceVersion: '2878854',
+      selfLink: '/apis/addon.open-cluster-management.io/v1alpha1/clustermanagementaddons/application-manager',
+      uid: '8abf6f17-ec7d-49e4-8390-a7319ff232d8',
     },
-    spec: { addOnConfigCRD: 'observability.agent.open-cluster-management.io', description: 'This is the tooltip for Observability', displayName: 'Observability' },
+    spec: { addOnConfiguration: { crName: '', crdName: 'klusterletaddonconfigs.agent.open-cluster-management.io' }, addOnMeta: { description: 'Processes events and other requests to managed resources.', displayName: 'Application Manager' } },
   }, {
     apiVersion: 'addon.open-cluster-management.io/v1alpha1',
     kind: 'ClusterManagementAddOn',
     metadata: {
-      creationTimestamp: '2020-08-31T17:50:51Z',
+      creationTimestamp: '2020-09-09T17:48:35Z',
       generation: 1,
       managedFields: [{
-        apiVersion: 'addon.open-cluster-management.io/v1alpha1',
-        fieldsType: 'FieldsV1',
-        fieldsV1: {
-          'f:spec': {
-            '.': {}, 'f:addOnConfigCRD': {}, 'f:description': {}, 'f:displayName': {},
-          },
-        },
-        manager: 'Mozilla',
-        operation: 'Update',
-        time: '2020-08-31T17:50:51Z',
+        apiVersion: 'addon.open-cluster-management.io/v1alpha1', fieldsType: 'FieldsV1', fieldsV1: { 'f:spec': { '.': {}, 'f:addOnConfiguration': { '.': {}, 'f:crName': {}, 'f:crdName': {} }, 'f:addOnMeta': { '.': {}, 'f:description': {}, 'f:displayName': {} } }, 'f:status': {} }, manager: 'endpoint-operator', operation: 'Update', time: '2020-09-09T17:48:35Z',
       }],
-      name: 'open-cluster-management-addon-application-manager',
-      resourceVersion: '5681832',
-      selfLink: '/apis/addon.open-cluster-management.io/v1alpha1/clustermanagementaddons/open-cluster-management-addon-application-manager',
-      uid: 'c58c532b-1d00-4aea-ad26-40df407fca6a',
+      name: 'cert-policy-controller',
+      resourceVersion: '2878855',
+      selfLink: '/apis/addon.open-cluster-management.io/v1alpha1/clustermanagementaddons/cert-policy-controller',
+      uid: '5b198c74-d4b0-4226-b284-c5707511a20d',
     },
-    spec: { addOnConfigCRD: 'klusterletaddonconfigs.agent.open-cluster-management.io', description: 'application manager', displayName: 'application-manager' },
+    spec: { addOnConfiguration: { crName: '', crdName: 'klusterletaddonconfigs.agent.open-cluster-management.io' }, addOnMeta: { description: 'Monitors certificate expiration based on distributed policies.', displayName: 'Cert Policy Controller' } },
+  }, {
+    apiVersion: 'addon.open-cluster-management.io/v1alpha1',
+    kind: 'ClusterManagementAddOn',
+    metadata: {
+      creationTimestamp: '2020-09-09T17:48:35Z',
+      generation: 1,
+      managedFields: [{
+        apiVersion: 'addon.open-cluster-management.io/v1alpha1', fieldsType: 'FieldsV1', fieldsV1: { 'f:spec': { '.': {}, 'f:addOnConfiguration': { '.': {}, 'f:crName': {}, 'f:crdName': {} }, 'f:addOnMeta': { '.': {}, 'f:description': {}, 'f:displayName': {} } }, 'f:status': {} }, manager: 'endpoint-operator', operation: 'Update', time: '2020-09-09T17:48:35Z',
+      }],
+      name: 'iam-policy-controller',
+      resourceVersion: '2878856',
+      selfLink: '/apis/addon.open-cluster-management.io/v1alpha1/clustermanagementaddons/iam-policy-controller',
+      uid: '666816c6-5ac9-47d3-8ac8-9297023df25c',
+    },
+    spec: { addOnConfiguration: { crName: '', crdName: 'klusterletaddonconfigs.agent.open-cluster-management.io' }, addOnMeta: { description: 'Monitors identity controls based on distributed policies.', displayName: 'IAM Policy Controller' } },
+  }, {
+    apiVersion: 'addon.open-cluster-management.io/v1alpha1',
+    kind: 'ClusterManagementAddOn',
+    metadata: {
+      creationTimestamp: '2020-09-09T17:48:35Z',
+      generation: 1,
+      managedFields: [{
+        apiVersion: 'addon.open-cluster-management.io/v1alpha1', fieldsType: 'FieldsV1', fieldsV1: { 'f:spec': { '.': {}, 'f:addOnConfiguration': { '.': {}, 'f:crName': {}, 'f:crdName': {} }, 'f:addOnMeta': { '.': {}, 'f:description': {}, 'f:displayName': {} } }, 'f:status': {} }, manager: 'endpoint-operator', operation: 'Update', time: '2020-09-09T17:48:35Z',
+      }],
+      name: 'policy-controller',
+      resourceVersion: '2878857',
+      selfLink: '/apis/addon.open-cluster-management.io/v1alpha1/clustermanagementaddons/policy-controller',
+      uid: 'c0286e45-12cd-47cb-bece-d323222eeb27',
+    },
+    spec: { addOnConfiguration: { crName: '', crdName: 'klusterletaddonconfigs.agent.open-cluster-management.io' }, addOnMeta: { description: 'Distributes configured policies and monitors Kubernetes-based policies.', displayName: 'Policy Controller' } },
+  }, {
+    apiVersion: 'addon.open-cluster-management.io/v1alpha1',
+    kind: 'ClusterManagementAddOn',
+    metadata: {
+      creationTimestamp: '2020-09-09T17:48:35Z',
+      generation: 1,
+      managedFields: [{
+        apiVersion: 'addon.open-cluster-management.io/v1alpha1', fieldsType: 'FieldsV1', fieldsV1: { 'f:spec': { '.': {}, 'f:addOnConfiguration': { '.': {}, 'f:crName': {}, 'f:crdName': {} }, 'f:addOnMeta': { '.': {}, 'f:description': {}, 'f:displayName': {} } }, 'f:status': {} }, manager: 'endpoint-operator', operation: 'Update', time: '2020-09-09T17:48:35Z',
+      }],
+      name: 'search-collector',
+      resourceVersion: '2878858',
+      selfLink: '/apis/addon.open-cluster-management.io/v1alpha1/clustermanagementaddons/search-collector',
+      uid: 'cdc2a1f6-7f64-4b8c-9405-337cfe57d963',
+    },
+    spec: { addOnConfiguration: { crName: '', crdName: 'klusterletaddonconfigs.agent.open-cluster-management.io' }, addOnMeta: { description: 'Collects cluster data to be indexed by search components on the hub cluster.', displayName: 'Search Collector' } },
+  }, {
+    apiVersion: 'addon.open-cluster-management.io/v1alpha1',
+    kind: 'ClusterManagementAddOn',
+    metadata: {
+      creationTimestamp: '2020-09-09T17:48:35Z',
+      generation: 1,
+      managedFields: [{
+        apiVersion: 'addon.open-cluster-management.io/v1alpha1', fieldsType: 'FieldsV1', fieldsV1: { 'f:spec': { '.': {}, 'f:addOnConfiguration': { '.': {}, 'f:crName': {}, 'f:crdName': {} }, 'f:addOnMeta': { '.': {}, 'f:description': {}, 'f:displayName': {} } }, 'f:status': {} }, manager: 'endpoint-operator', operation: 'Update', time: '2020-09-09T17:48:35Z',
+      }],
+      name: 'work-manager',
+      resourceVersion: '2878859',
+      selfLink: '/apis/addon.open-cluster-management.io/v1alpha1/clustermanagementaddons/work-manager',
+      uid: '954e0672-1e2f-47eb-bfc6-c3c730e1edd1',
+    },
+    spec: { addOnConfiguration: { crName: '', crdName: 'klusterletaddonconfigs.agent.open-cluster-management.io' }, addOnMeta: { description: 'Component that handles endpoint work requests and managed cluster status.', displayName: 'Work Manager' } },
   }],
   kind: 'ClusterManagementAddOnList',
-  metadata: { continue: '', resourceVersion: '5947757', selfLink: '/apis/addon.open-cluster-management.io/v1alpha1/clustermanagementaddons' },
+  metadata: { continue: '', resourceVersion: '2924924', selfLink: '/apis/addon.open-cluster-management.io/v1alpha1/clustermanagementaddons' },
 };
 
 export default mockResponse;
--- src/v2/mocks/ManagedClusterAddons.js
+++ src/v2/mocks/ManagedClusterAddons.js
@@ -5,89 +5,244 @@
 
 const mockResponse = {
   apiVersion: 'addon.open-cluster-management.io/v1alpha1',
-  kind: 'ManagedClusterAddOnList',
-  metadata: {
-    continue: '',
-    resourceVersion: '5933793',
-    selfLink: '/apis/addon.open-cluster-management.io/v1alpha1/namespaces/hub-cluster/managedclusteraddons',
-  },
-  items: [
-    {
-      apiVersion: 'addon.open-cluster-management.io/v1alpha1',
-      kind: 'ManagedClusterAddOn',
-      metadata: {
-        creationTimestamp: '2020-08-31T17:51:07Z',
-        generation: 2,
-        managedFields: [
-          {
-            apiVersion: 'addon.open-cluster-management.io/v1alpha1',
-            fieldsType: 'FieldsV1',
-            fieldsV1: {
-              'f:spec': {
-
-              },
-              'f:status': {
-                '.': {
-
-                },
-                'f:addOnResource': {
-                  '.': {
-
-                  },
-                  'f:group': {
-
-                  },
-                  'f:name': {
-
-                  },
-                  'f:resource': {
-
-                  },
-                },
-                'f:conditions': {
-
-                },
+  items: [{
+    apiVersion: 'addon.open-cluster-management.io/v1alpha1',
+    kind: 'ManagedClusterAddOn',
+    metadata: {
+      creationTimestamp: '2020-09-09T17:53:25Z',
+      generation: 1,
+      managedFields: [{
+        apiVersion: 'addon.open-cluster-management.io/v1alpha1',
+        fieldsType: 'FieldsV1',
+        fieldsV1: {
+          'f:metadata': {
+            'f:ownerReferences': {
+              '.': {},
+              'k:{"uid":"f6dedd38-2973-40a5-8dcb-d2a055359156"}': {
+                '.': {}, 'f:apiVersion': {}, 'f:blockOwnerDeletion': {}, 'f:controller': {}, 'f:kind': {}, 'f:name': {}, 'f:uid': {},
               },
             },
-            manager: 'Mozilla',
-            operation: 'Update',
-            time: '2020-08-31T21:48:31Z',
           },
-        ],
-        name: 'open-cluster-management-addon-application-manager',
-        namespace: 'hub-cluster',
-        resourceVersion: '5814291',
-        selfLink: '/apis/addon.open-cluster-management.io/v1alpha1/namespaces/hub-cluster/managedclusteraddons/open-cluster-management-addon-application-manager',
-        uid: '7096881e-363d-4a7f-9c37-364451cc6571',
-      },
-      spec: {
-
-      },
-      status: {
-        addOnResource: {
-          group: 'agent.open-cluster-management.io',
-          name: 'test-cluster',
-          resource: 'klusterletaddonconfigs',
+          'f:spec': {},
+          'f:status': {
+            '.': {}, 'f:addOnConfiguration': { '.': {}, 'f:crName': {}, 'f:crdName': {} }, 'f:addOnMeta': { '.': {}, 'f:description': {}, 'f:displayName': {} }, 'f:conditions': {}, 'f:relatedObjects': {},
+          },
         },
-        conditions: [
-          {
-            lastTransitionTime: '2020-08-31T14:55:25Z',
-            message: 'manifestwork applying',
-            reason: 'ManifestWorkApplying',
-            status: 'True',
-            type: 'Progressing',
-          },
-          {
-            lastTransitionTime: '2020-08-31T14:58:25Z',
-            message: 'addon is not ready',
-            reason: 'AddOnNotReady',
-            status: 'False',
-            type: 'Available',
-          },
-        ],
-      },
+        manager: 'endpoint-operator',
+        operation: 'Update',
+        time: '2020-09-09T19:11:23Z',
+      }],
+      name: 'application-manager',
+      namespace: 'han-test',
+      ownerReferences: [{
+        apiVersion: 'agent.open-cluster-management.io/v1', blockOwnerDeletion: true, controller: true, kind: 'KlusterletAddonConfig', name: 'han-test', uid: 'f6dedd38-2973-40a5-8dcb-d2a055359156',
+      }],
+      resourceVersion: '2920509',
+      selfLink: '/apis/addon.open-cluster-management.io/v1alpha1/namespaces/han-test/managedclusteraddons/application-manager',
+      uid: 'f7678b07-1286-4437-8c2b-2971dd2b7956',
     },
-  ],
+    spec: {},
+    status: {
+      addOnConfiguration: { crName: '', crdName: '' },
+      addOnMeta: { description: '', displayName: '' },
+      conditions: [{
+        lastTransitionTime: '2020-09-09T18:15:53Z', message: 'Applied ManifestWork', reason: 'ManifestWorkApplied', status: 'False', type: 'Progressing',
+      }, {
+        lastTransitionTime: '2020-09-09T19:11:23Z', message: 'Addon is available', reason: 'AddonAvailable', status: 'True', type: 'Available',
+      }],
+      relatedObjects: [{ group: 'agent.open-cluster-management.io', name: 'han-test', resource: 'klusterletaddonconfigs' }],
+    },
+  }, {
+    apiVersion: 'addon.open-cluster-management.io/v1alpha1',
+    kind: 'ManagedClusterAddOn',
+    metadata: {
+      creationTimestamp: '2020-09-09T17:53:25Z',
+      generation: 1,
+      managedFields: [{
+        apiVersion: 'addon.open-cluster-management.io/v1alpha1',
+        fieldsType: 'FieldsV1',
+        fieldsV1: {
+          'f:metadata': {
+            'f:ownerReferences': {
+              '.': {},
+              'k:{"uid":"f6dedd38-2973-40a5-8dcb-d2a055359156"}': {
+                '.': {}, 'f:apiVersion': {}, 'f:blockOwnerDeletion': {}, 'f:controller': {}, 'f:kind': {}, 'f:name': {}, 'f:uid': {},
+              },
+            },
+          },
+          'f:spec': {},
+          'f:status': {
+            '.': {}, 'f:addOnConfiguration': { '.': {}, 'f:crName': {}, 'f:crdName': {} }, 'f:addOnMeta': { '.': {}, 'f:description': {}, 'f:displayName': {} }, 'f:conditions': {}, 'f:relatedObjects': {},
+          },
+        },
+        manager: 'endpoint-operator',
+        operation: 'Update',
+        time: '2020-09-09T19:01:40Z',
+      }],
+      name: 'cert-policy-controller',
+      namespace: 'han-test',
+      ownerReferences: [{
+        apiVersion: 'agent.open-cluster-management.io/v1', blockOwnerDeletion: true, controller: true, kind: 'KlusterletAddonConfig', name: 'han-test', uid: 'f6dedd38-2973-40a5-8dcb-d2a055359156',
+      }],
+      resourceVersion: '2915730',
+      selfLink: '/apis/addon.open-cluster-management.io/v1alpha1/namespaces/han-test/managedclusteraddons/cert-policy-controller',
+      uid: '8d513d73-93c4-441f-a186-66788081ee30',
+    },
+    spec: {},
+    status: {
+      addOnConfiguration: { crName: '', crdName: '' },
+      addOnMeta: { description: '', displayName: '' },
+      conditions: [{
+        lastTransitionTime: '2020-09-09T18:15:53Z', message: 'Applied ManifestWork', reason: 'ManifestWorkApplied', status: 'False', type: 'Progressing',
+      }, {
+        lastTransitionTime: '2020-09-09T19:01:40Z', message: 'Addon is available', reason: 'AddonAvailable', status: 'True', type: 'Available',
+      }],
+      relatedObjects: [{ group: 'agent.open-cluster-management.io', name: 'han-test', resource: 'klusterletaddonconfigs' }],
+    },
+  }, {
+    apiVersion: 'addon.open-cluster-management.io/v1alpha1',
+    kind: 'ManagedClusterAddOn',
+    metadata: {
+      creationTimestamp: '2020-09-09T17:53:26Z',
+      generation: 1,
+      managedFields: [{
+        apiVersion: 'addon.open-cluster-management.io/v1alpha1',
+        fieldsType: 'FieldsV1',
+        fieldsV1: {
+          'f:metadata': {
+            'f:ownerReferences': {
+              '.': {},
+              'k:{"uid":"f6dedd38-2973-40a5-8dcb-d2a055359156"}': {
+                '.': {}, 'f:apiVersion': {}, 'f:blockOwnerDeletion': {}, 'f:controller': {}, 'f:kind': {}, 'f:name': {}, 'f:uid': {},
+              },
+            },
+          },
+          'f:spec': {},
+          'f:status': {
+            '.': {}, 'f:addOnConfiguration': { '.': {}, 'f:crName': {}, 'f:crdName': {} }, 'f:addOnMeta': { '.': {}, 'f:description': {}, 'f:displayName': {} }, 'f:conditions': {}, 'f:relatedObjects': {},
+          },
+        },
+        manager: 'endpoint-operator',
+        operation: 'Update',
+        time: '2020-09-09T19:11:20Z',
+      }],
+      name: 'policy-controller',
+      namespace: 'han-test',
+      ownerReferences: [{
+        apiVersion: 'agent.open-cluster-management.io/v1', blockOwnerDeletion: true, controller: true, kind: 'KlusterletAddonConfig', name: 'han-test', uid: 'f6dedd38-2973-40a5-8dcb-d2a055359156',
+      }],
+      resourceVersion: '2920481',
+      selfLink: '/apis/addon.open-cluster-management.io/v1alpha1/namespaces/han-test/managedclusteraddons/policy-controller',
+      uid: '9ef60e69-3645-4a8e-bb5e-007bf92fbee7',
+    },
+    spec: {},
+    status: {
+      addOnConfiguration: { crName: '', crdName: '' },
+      addOnMeta: { description: '', displayName: '' },
+      conditions: [{
+        lastTransitionTime: '2020-09-09T18:15:51Z', message: 'Applied ManifestWork', reason: 'ManifestWorkApplied', status: 'False', type: 'Progressing',
+      }, {
+        lastTransitionTime: '2020-09-09T19:11:20Z', message: 'Addon is available', reason: 'AddonAvailable', status: 'True', type: 'Available',
+      }],
+      relatedObjects: [{ group: 'agent.open-cluster-management.io', name: 'han-test', resource: 'klusterletaddonconfigs' }],
+    },
+  }, {
+    apiVersion: 'addon.open-cluster-management.io/v1alpha1',
+    kind: 'ManagedClusterAddOn',
+    metadata: {
+      creationTimestamp: '2020-09-09T19:01:44Z',
+      generation: 1,
+      managedFields: [{
+        apiVersion: 'addon.open-cluster-management.io/v1alpha1',
+        fieldsType: 'FieldsV1',
+        fieldsV1: {
+          'f:metadata': {
+            'f:ownerReferences': {
+              '.': {},
+              'k:{"uid":"f6dedd38-2973-40a5-8dcb-d2a055359156"}': {
+                '.': {}, 'f:apiVersion': {}, 'f:blockOwnerDeletion': {}, 'f:controller': {}, 'f:kind': {}, 'f:name': {}, 'f:uid': {},
+              },
+            },
+          },
+          'f:spec': {},
+          'f:status': {
+            '.': {}, 'f:addOnConfiguration': { '.': {}, 'f:crName': {}, 'f:crdName': {} }, 'f:addOnMeta': { '.': {}, 'f:description': {}, 'f:displayName': {} }, 'f:conditions': {}, 'f:relatedObjects': {},
+          },
+        },
+        manager: 'endpoint-operator',
+        operation: 'Update',
+        time: '2020-09-09T19:01:51Z',
+      }],
+      name: 'search-collector',
+      namespace: 'han-test',
+      ownerReferences: [{
+        apiVersion: 'agent.open-cluster-management.io/v1', blockOwnerDeletion: true, controller: true, kind: 'KlusterletAddonConfig', name: 'han-test', uid: 'f6dedd38-2973-40a5-8dcb-d2a055359156',
+      }],
+      resourceVersion: '2915826',
+      selfLink: '/apis/addon.open-cluster-management.io/v1alpha1/namespaces/han-test/managedclusteraddons/search-collector',
+      uid: '485bd9d9-6f8e-4fae-b3fa-7c2461d7a528',
+    },
+    spec: {},
+    status: {
+      addOnConfiguration: { crName: '', crdName: '' },
+      addOnMeta: { description: '', displayName: '' },
+      conditions: [{
+        lastTransitionTime: '2020-09-09T19:01:51Z', message: 'Applied ManifestWork', reason: 'ManifestWorkApplied', status: 'False', type: 'Progressing',
+      }, {
+        lastTransitionTime: '2020-09-09T19:01:44Z', message: 'Addon is not available', reason: 'AddonNotReady', status: 'False', type: 'Available',
+      }],
+      relatedObjects: [{ group: 'agent.open-cluster-management.io', name: 'han-test', resource: 'klusterletaddonconfigs' }],
+    },
+  }, {
+    apiVersion: 'addon.open-cluster-management.io/v1alpha1',
+    kind: 'ManagedClusterAddOn',
+    metadata: {
+      creationTimestamp: '2020-09-09T17:53:29Z',
+      generation: 1,
+      managedFields: [{
+        apiVersion: 'addon.open-cluster-management.io/v1alpha1',
+        fieldsType: 'FieldsV1',
+        fieldsV1: {
+          'f:metadata': {
+            'f:ownerReferences': {
+              '.': {},
+              'k:{"uid":"f6dedd38-2973-40a5-8dcb-d2a055359156"}': {
+                '.': {}, 'f:apiVersion': {}, 'f:blockOwnerDeletion': {}, 'f:controller': {}, 'f:kind': {}, 'f:name': {}, 'f:uid': {},
+              },
+            },
+          },
+          'f:spec': {},
+          'f:status': {
+            '.': {}, 'f:addOnConfiguration': { '.': {}, 'f:crName': {}, 'f:crdName': {} }, 'f:addOnMeta': { '.': {}, 'f:description': {}, 'f:displayName': {} }, 'f:conditions': {}, 'f:relatedObjects': {},
+          },
+        },
+        manager: 'endpoint-operator',
+        operation: 'Update',
+        time: '2020-09-09T19:11:20Z',
+      }],
+      name: 'work-manager',
+      namespace: 'han-test',
+      ownerReferences: [{
+        apiVersion: 'agent.open-cluster-management.io/v1', blockOwnerDeletion: true, controller: true, kind: 'KlusterletAddonConfig', name: 'han-test', uid: 'f6dedd38-2973-40a5-8dcb-d2a055359156',
+      }],
+      resourceVersion: '2920479',
+      selfLink: '/apis/addon.open-cluster-management.io/v1alpha1/namespaces/han-test/managedclusteraddons/work-manager',
+      uid: '9a152b81-e537-4b8c-9451-afc9816da6ce',
+    },
+    spec: {},
+    status: {
+      addOnConfiguration: { crName: '', crdName: '' },
+      addOnMeta: { description: '', displayName: '' },
+      conditions: [{
+        lastTransitionTime: '2020-09-09T18:15:50Z', message: 'Applied ManifestWork', reason: 'ManifestWorkApplied', status: 'False', type: 'Progressing',
+      }, {
+        lastTransitionTime: '2020-09-09T19:11:20Z', message: 'Addon is available', reason: 'AddonAvailable', status: 'True', type: 'Available',
+      }],
+      relatedObjects: [{ group: 'agent.open-cluster-management.io', name: 'han-test', resource: 'klusterletaddonconfigs' }],
+    },
+  }],
+  kind: 'ManagedClusterAddOnList',
+  metadata: { continue: '', resourceVersion: '2925619', selfLink: '/apis/addon.open-cluster-management.io/v1alpha1/namespaces/han-test/managedclusteraddons' },
 };
 
 export default mockResponse;
