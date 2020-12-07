6b86e7fe8a8a87fbb1521495ff420ac4ac4eff1b
sahare <sahare@redhat.com>
2020-12-07 16:11:55 -0500

    Try to create namespaces even if user can not list namespaces

--- src/v2/models/application.js
+++ src/v2/models/application.js
@@ -368,12 +368,11 @@ export default class ApplicationModel extends KubeModel {
     }
 
     const existingNamespaces = await this.kubeConnector.get('/api/v1/namespaces');
-    checkAndCollectError(existingNamespaces);
-    if (errors.length !== 0) {
-      return { errors };
-    }
     const namespaceResponses = await Promise.all(namespaces.map((ns) => {
-      const namespaceExists = existingNamespaces.items.find((existingNamespace) => existingNamespace.metadata.name === ns);
+      let namespaceExists = false;
+      if (existingNamespaces && existingNamespaces.items) {
+        namespaceExists = existingNamespaces.items.find((existingNamespace) => existingNamespace.metadata.name === ns);
+      }
       return !namespaceExists ? this.createNamespace(ns) : undefined;
     }));
 
