2be75b639761a17cd75f6c548d6beeea1cc35d49
Feng Xiang <fxiang@redhat.com>
2021-02-09 16:38:54 -0500

    Fix copyright info

--- src/v2/schema/application-helper-util.js
+++ src/v2/schema/application-helper-util.js
@@ -1,10 +1,4 @@
 /** *****************************************************************************
- * Licensed Materials - Property of IBM
- * (c) Copyright IBM Corporation 2019. All Rights Reserved.
- * Copyright (c) 2020 Red Hat, Inc.
- * Note to U.S. Government Users Restricted Rights:
- * Use, duplication or disclosure restricted by GSA ADP Schedule
- * Contract with IBM Corp.
  * Copyright (c) 2021 Red Hat, Inc.
  ****************************************************************************** */
 /* eslint no-param-reassign: "error" */
