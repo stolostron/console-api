ebc26b3f5415db873b3ae6b71364ab8d0c7729fd
Kevin Cormier <kcormier@redhat.com>
2021-03-18 09:41:35 -0400

    Use security-middleware from npmjs.com

--- src/v2/index.js
+++ src/v2/index.js
@@ -16,7 +16,7 @@ import helmet from 'helmet';
 import noCache from 'nocache';
 import compression from 'compression';
 import cookieParser from 'cookie-parser';
-import inspect from 'security-middleware';
+import inspect from '@open-cluster-management/security-middleware';
 import _ from 'lodash';
 
 import logger from './lib/logger';
